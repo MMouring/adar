a17ce6519f64603426a0c108a3115ba6e089e3e6
Your Name <you@example.com>
2025-03-03 11:56:05 -0500

    tmp

--- src/utils/aws/s3.py
+++ src/utils/aws/s3.py
@@ -1,18 +1,19 @@
 from botocore.exceptions import ClientError
 import boto3
 
+
 class S3Operations:
-    def __init__(self):
+    def __init__(self, region_name: str = 'us-east-1', bucket: str = ''):
         self.s3_client = boto3.client('s3')
-        self.BUCKET = 'hotelmedia'
+        self.BUCKET = bucket
 
     def head_object(self, key: str) -> dict:
         """
         Get metadata about an object in S3 without downloading it
-        
+
         Args:
             key (str): The key (path) of the object in S3
-            
+
         Returns:
             dict: Object metadata if successful, None if object doesn't exist
         """
@@ -29,13 +30,13 @@ class S3Operations:
     def put_object(self, object_params: dict) -> dict:
         """
         Upload an object to S3
-        
+
         Args:
             object_params (dict): Parameters for the upload including Key and Body
-            
+
         Returns:
             dict: Response from S3 if successful
-            
+
         Raises:
             ClientError: If upload fails
         """
@@ -43,6 +44,6 @@ class S3Operations:
             object_params['Bucket'] = self.BUCKET
             response = self.s3_client.put_object(**object_params)
             return response
-        except ClientError as e:
+        except Exception as e:
             print(e)
             raise
